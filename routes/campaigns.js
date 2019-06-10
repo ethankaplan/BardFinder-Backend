9352554a768e977c4d36add1183ce06fb268d779
Ethan <N/A>
2019-06-10 02:03:03 -0700

    Fix owner showing in view campaign

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -33,7 +33,7 @@ router.delete('/:id/:owner', async(req,res)=>{
     }
 })
 
-router.put('/:id', async(req,res)=>{
+router.put('/e/:id', async(req,res)=>{
                              
         try {
             console.log(req.body)
@@ -87,7 +87,7 @@ router.put('/:id', async(req,res)=>{
 
     router.get('/:id', async (req,res)=>{
         try{
-            const campaign = await Campaign.findById(req.params.id)
+            const campaign = await Campaign.findById(req.params.id).populate("owner")
             
             res.json({
                 campaign
